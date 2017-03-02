84bde984f51c0b9fdeecc150089d6316ab11ed66
Aron Feher <feher@tyntec.com>
2017-03-02 16:17:30 +0100

    Adjusted default area size.

--- src/main/java/snake/Main.java
+++ src/main/java/snake/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Map<String, String> settings = new HashMap<>();
         settings.put("scale", "12");
-        settings.put("height", "30");
-        settings.put("width", "30");
+        settings.put("height", "40");
+        settings.put("width", "40");
         Game game = new Game(settings);
         game.run();
     }
